eb79ec53b904943500fbd8b16ffb4ef22c6ef680
Emika Ijuin <emikaijuin@gmail.com>
2019-02-15 13:49:53 +0800

    add flask_login login manager and load_user callback

--- app.py
+++ app.py
@@ -3,13 +3,17 @@ import config
 from flask import Flask
 from models.base_model import db
 from flask_wtf.csrf import CSRFProtect
-
+from flask_login import LoginManager
+from models.user import User
 
 web_dir = os.path.join(os.path.dirname(
     os.path.abspath(__file__)), 'instagram_web')
 
 app = Flask('NEXTAGRAM', root_path=web_dir)
+
 csrf = CSRFProtect(app)
+login_manager = LoginManager()
+login_manager.init_app(app)
 
 if os.getenv('FLASK_ENV') == 'production':
     app.config.from_object("config.ProductionConfig")
@@ -26,3 +30,7 @@ def before_request():
 def after_request(response):
     db.close()
     return response
+
+@login_manager.user_loader
+def load_user(user_id):
+    return User.get_or_none(id=user_id)
