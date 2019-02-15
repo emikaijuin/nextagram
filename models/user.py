7dbff74346353dceeb67b0066c41d71bac98464d
Emika Ijuin <emikaijuin@gmail.com>
2019-02-15 13:49:02 +0800

    add required functions for flask-login

--- models/user.py
+++ models/user.py
@@ -15,3 +15,15 @@ class User(BaseModel):
             self.errors.append("Email is already taken, please login with existing email.")
         if duplicate_username:
             self.errors.append("Username is already taken, please try creating another.")
+
+    def is_authenticated(self):
+        return True
+
+    def is_active(self):
+        return True
+
+    def is_anonymous(self):
+        return True
+    
+    def get_id(self):
+        return str(self.id)
