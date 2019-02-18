266bff3c8c5d243f163dd1b2c43a186a0edf158c
Emika Ijuin <emikaijuin@gmail.com>
2019-02-18 15:57:11 +0800

    set blueprint template as 'templates', specify sub folder in render_template calls

--- instagram_web/blueprints/images/views.py
+++ instagram_web/blueprints/images/views.py
@@ -1,10 +1,11 @@
 from flask import Blueprint, render_template, request, redirect, url_for, flash
 from models.image import *
 from models.user import *
+from flask_login import current_user
 
 images_blueprint = Blueprint('images',
                             __name__,
-                            template_folder="templates/images")
+                            template_folder="templates")
 
 @images_blueprint.route("/new", methods=["GET"])
 def new():
