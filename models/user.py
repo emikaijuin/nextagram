c57df04f8e07beb56eef689dc11c45c4665142bb
Emika Ijuin <emikaijuin@gmail.com>
2019-02-18 14:18:16 +0800

    User Profile Picture Upload
    - Add column Avatar with type String to User table
    - Upload image to S3, return [success (True or False), "Error message when False"]
    - Use peewee playhouse hybrid module to create return profile_image_url by appending file name to Bucket location stored in app environment vars.

--- models/user.py
+++ models/user.py
@@ -1,11 +1,13 @@
 from models.base_model import BaseModel
 import peewee as pw
-
-
+from playhouse.hybrid import hybrid_property
+from werkzeug.utils import secure_filename
+from helpers import *
 class User(BaseModel):
     username = pw.CharField(unique=True)
     email = pw.CharField(unique=True, index=True)
     password_digest = pw.CharField(unique=False)
+    avatar = pw.CharField(null=True)
 
     def validate(self):
         duplicate_email = User.get_or_none(User.email == self.email)
@@ -27,3 +29,27 @@ class User(BaseModel):
     
     def get_id(self):
         return str(self.id)
+
+    def upload_file(self, request):
+      if "user_file" not in request.files:
+        return [False, "No user_file key in request.files"]
+      file = request.files["user_file"]
+
+      if file.filename == "":
+        return [False, "Please select a file"]
+
+      if file:
+        file.filename = secure_filename(file.filename)
+        output = upload_file_to_s3(file, S3_BUCKET)
+        return [True, str(output)]
+
+      else:
+        return [False, "Avatar could not be uploaded to Amazon."]
+
+    @hybrid_property
+    def profile_image_url(self):
+      if self.avatar:
+        return S3_LOCATION + self.avatar
+      else:
+        return ""
+        
