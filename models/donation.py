f838835ca93ed660283089b878b4d9113988af6c
Emika Ijuin <emikaijuin@gmail.com>
2019-02-19 15:57:59 +0800

    use sendgrid to send email

--- models/donation.py
+++ models/donation.py
@@ -2,8 +2,11 @@ from models.base_model import BaseModel
 from models.user import User
 from models.image import Image
 import peewee as pw
-from config import BRAINTREE_MERCHANT_ID, BRAINTREE_PUBLIC_KEY, BRAINTREE_PRIVATE_KEY
+from config import BRAINTREE_MERCHANT_ID, BRAINTREE_PUBLIC_KEY, BRAINTREE_PRIVATE_KEY, SENDGRID_PRIVATE_KEY
 import braintree
+import sendgrid
+import os
+from sendgrid.helpers.mail import *
 
 gateway = braintree.BraintreeGateway(
   braintree.Configuration(
@@ -33,3 +36,14 @@ class Donation(BaseModel):
     })
 
     return result
+
+  def notify_recipient(self, donor_name, amount, message):
+    print("!!!!!!!!!!!!!!!!!!!!!!!!")
+    print(SENDGRID_PRIVATE_KEY)
+    sg = sendgrid.SendGridAPIClient(apikey= SENDGRID_PRIVATE_KEY)
+    from_email = Email("test@example.com")
+    to_email = Email("emikaijuin@gmail.com")
+    subject = "You received a donation!"
+    content = Content("text/plain", f"You received ${amount} from {donor_name}! They said: {message}")
+    mail = Mail(from_email, subject, to_email, content)
+    response = sg.client.mail.send.post(request_body=mail.get())
