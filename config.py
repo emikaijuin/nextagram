3b695a8c97e88b883246776abac9ff752fdced8b
Emika Ijuin <emikaijuin@gmail.com>
2019-02-20 11:12:28 +0800

    add google oauth info to config

--- config.py
+++ config.py
@@ -1,12 +1,17 @@
 import os
 
-
 class Config(object):
     DEBUG = False
     TESTING = False
     CSRF_ENABLED = True
     SECRET_KEY = os.environ.get(
         'SECRET_KEY') or os.urandom(32)
+    GOOGLE_CLIENT_ID = os.environ.get(
+        'GOOGLE_CLIENT_ID'
+    )
+    GOOGLE_CLIENT_SECRET = os.environ.get(
+        'GOOGLE_CLIENT_SECRET'
+    )
 
 
 class ProductionConfig(Config):
