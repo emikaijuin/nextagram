c7f0abc9d1aef129d6b64c278c5814b01f0c75e1
Emika Ijuin <emikaijuin@gmail.com>
2019-02-20 12:07:48 +0800

    User can log in with google

--- instagram_web/blueprints/sessions/views.py
+++ instagram_web/blueprints/sessions/views.py
@@ -9,7 +9,12 @@ from flask_login import login_user, current_user, logout_user
 sessions_blueprint = Blueprint('sessions',
                             __name__,
                             template_folder='templates')
+from instagram_web.helpers.google_oauth import oauth
 
+@sessions_blueprint.route("/new_oauth/<provider>", methods=["GET"])
+def new_oauth(provider):
+  redirect_uri = url_for("sessions.authorize", provider=provider, _external = True)
+  return oauth.google.authorize_redirect(redirect_uri)
 
 @sessions_blueprint.route("/new", methods=["GET"])
 def new():
@@ -35,3 +40,20 @@ def destroy():
   logout_user()
   flash(f"You signed out.")
   return redirect(url_for('home'))
+
+@sessions_blueprint.route('/authorize/<provider>', methods=["GET"])
+def authorize(provider):
+  if provider == "google":
+    token = oauth.google.authorize_access_token()
+    email = oauth.google.get('https://www.googleapis.com/oauth2/v2/userinfo').json()['email']
+  
+  try:
+    user = User.get(email = email)
+    login_user(user)
+    flash(f"Welcome, {user.username}.")
+    return redirect(url_for('users.show', username = user.username))
+  except:
+    flash("Sorry, we weren't able to find a user with that email.")
+    return redirect("/")
+
+  
