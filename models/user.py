18aca86a03b7d2615c732615259ab55d4a47204c
Emika Ijuin <emikaijuin@gmail.com>
2019-02-20 17:16:50 +0800

    To avoid circular imports between User and Relationship table, relationship class was added into User model file.
    User Model:
    
    Users model functions:
      - Follow other users
      - Get list of their followers
      - Get list of who they are following
      - Unfollow a user
      - Check if useris already following another uesr
    
    Above methods are instance functions on a user object and require username of a second user. Username is argument for second user because it is the id in the user routes, using username avoids requiring to do a database search to run these methods. In this set up, performance can be improved later by refactoring the requests to AJAX requests or by triggering jobs.
    
    Relationship model:
      - Add boolean column 'approved' to differentiate betwen a pending follow request and an approved follower
      - Relationships can be approved
      - A list of unapproved relationships (Follow requests) can be returned

--- models/user.py
+++ models/user.py
@@ -58,4 +58,88 @@ class User(BaseModel):
         return s3_url(self.avatar)
       else:
         return ""
-        
+
+    # Following
+
+    def follow(self, followee):
+      follow = Relationship(
+          follower = self.id,
+          following = followee.id
+      )
+
+      follow.save()
+
+    def followers(self):
+      return (User
+              .select()
+              .join(Relationship, on=(Relationship.follower_id == User.id))
+              .where(Relationship.following == self.id)
+            )
+
+    def following(self):
+        return (User
+                .select()
+                .join(Relationship, on=(Relationship.following_id == User.id))
+                .where(Relationship.follower == self.id)
+            )
+
+    def follow(self, username):
+        user = User.get(username = username)
+        relationship = Relationship(
+            follower_id = self.id,
+            following_id = user.id
+        )
+        relationship.save()
+
+    def unfollow(self, username):
+        user = User.get(username = username)
+        relationship = Relationship.delete().where(
+            Relationship.follower_id == self.id,
+            Relationship.following_id == user.id
+        )
+        relationship.execute()
+
+    def is_following(self, username):
+        user = User.get(username = username)
+        return Relationship.get_or_none(
+            follower_id = self.id,
+            following_id = user.id,
+            approved = True
+        )
+
+    def can_view(self, username):
+        user = User.get(username = username)
+        if self == user:
+            return True
+        if not user.private:
+            return True
+        if self.is_following(username):
+            return True
+        return False
+
+    def has_pending_request_to(self, username):
+      user = User.get(username = username)
+      relationship = Relationship.get_or_none(
+        follower_id = self.id, 
+        following_id = user.id
+      )
+      if relationship and relationship.approved == False:
+        return True
+      else:
+        return False
+
+class Relationship(BaseModel):
+  follower = pw.ForeignKeyField(User)
+  following = pw.ForeignKeyField(User)
+  approved = pw.BooleanField(default=False)
+
+  def approve(self):
+      self.approved = True
+      self.save()
+
+  @classmethod
+  def unapproved_requests(cls, user):
+      return Relationship.select().where(
+          Relationship.following_id == user.id,
+          Relationship.approved == False
+      )
